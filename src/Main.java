e4195f0b1ba3215e24a160118fc3842f20755190
Marina.Sachko <marina.sachko@mail.ru>
2023-05-04 07:30:18 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice;
-        int oneMily;
-        int mily;
 
-        oneMily = 20;                   // количество рублей для одной бонусной мили
-        ticketPrice = 13676;            // цена билета
-
-        mily = ticketPrice / oneMily;    // количество бонусных миль
+        int oneMily = 20;                   // количество рублей для одной бонусной мили
+        int ticketPrice = 13676;            // цена билета
+        int mily = ticketPrice / oneMily;    // количество бонусных миль
 
         System.out.println("Бесплатные бонусные мили = " + mily);
     }
